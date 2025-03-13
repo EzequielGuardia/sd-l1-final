92e2d46eac91ff10c3aee81f5a3c6414d2567bad
Ezequiel Guardia <julguardia@gmail.com>
2025-03-13 00:10:31 -0300

    feature:se aniadio @types/jsonfile para que lo puedad utilizar typescript ademas de aniadirse el modelo de add poeli para almacenar un array de pelicular en la base de datos json

--- src/models.ts
+++ src/models.ts
@@ -1,4 +1,4 @@
-import * as jsonfile from "jsonfile";
+import * as jsonfile from 'jsonfile';
 // El siguiente import no se usa pero es necesario
 import "./pelis.json";
 // de esta forma Typescript se entera que tiene que incluir
@@ -6,6 +6,8 @@ import "./pelis.json";
 // si no, solo usandolo desde la libreria jsonfile, no se dá cuenta
 
 // no modificar estas propiedades, agregar todas las que quieras
+
+
 class Peli {
   id: number;
   title: string;
@@ -27,20 +29,45 @@ class PelisCollection {
   getByid(id: number): Promise<Peli | null> {
     return this.getAll().then((pelis: Peli[]) => {
       const peliexistente = pelis.find(peli => peli.id === id)
-      return peliexistente || null
+      return peliexistente
     }).catch((error) => {
       console.log("no se ha podido leer el archivo correctamente");
       return null
     });
-
-    } 
-  
-    
   }
 
+    addPeli(pelicula: Peli): Promise<Boolean> {
+      const promesa1 = this.getByid(pelicula.id).then((peliexistente) => 
+      { if (peliexistente) {
+        return false
+      } else {
+
+
+        const data = {
+          peliculas: [] as Peli[],
+        }
+
+        data.peliculas.push(pelicula);
+
+       const promesa2 = jsonfile.writeFile("./pelis.json", data);
+        return promesa2.then((resolve)=> {
+            return true
+        });
+      }
+    });
+    
+    return promesa1
+    
+    } 
+
+    
+  
+    
+    
+
 
   
   
 
-
+  }
 export { PelisCollection, Peli };
