291a983ef64f38a450e71d243bcc405d096f45cd
Ezequiel Guardia <julguardia@gmail.com>
2025-03-18 20:57:41 -0300

    nose que commit hice pero esta

--- src/controllers.ts
+++ src/controllers.ts
@@ -15,14 +15,16 @@ class PelisController {
   }
 
   async get(options?: Options): Promise<Peli[]> {
-    if (options.id?) {
+    if (options?.id) {
       const peli = await this.model.getByid(options.id);
       return peli ? [peli]: [];
     } else if (options?.search) {
-      return this.model
+      return this.model.search(options.search)
+  } else {
+     return this.model.getAll();
   }
 
-
+  }
 
 }
 export { PelisController };
